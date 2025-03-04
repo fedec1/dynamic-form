61b85494dafc162a5553fe0890824c310ac34f81
fedec1 <colantuono.federico.w@gmail.com>
2025-03-04 12:21:45 +0100

    key press

--- dynamic-form-2-il-ritorno/public/js/home.js
+++ dynamic-form-2-il-ritorno/public/js/home.js
@@ -1,3 +1,6 @@
+// source code per il syntax highlighting : 
+// https://www.codeproject.com/Articles/5361561/Syntax-Highlightning-for-Textarea-HTML
+
 tArea = document.getElementById('jsonForm')
 preCode = document.getElementById('showCode')
 example = document.getElementById('example')
@@ -23,9 +26,71 @@ tArea.addEventListener("input", () => {
 })
 
 tArea.addEventListener("scroll", () => {
-    codeBlock.scrollTop = textarea1.scrollTop;
-    codeBlock.scrollLeft = textarea1.scrollLeft;
+    preCode.scrollTop = textarea1.scrollTop;
+    preCode.scrollLeft = textarea1.scrollLeft;
 });
+
+tArea.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        let cursorPos = tArea.selectionStart;
+        let prevLine = tArea.value.substring(0, cursorPos).split('\n').slice(-1)[0];
+        let indent = prevLine.match(/^\s*/)[0];
+        tArea.setRangeText('\n' + indent, cursorPos, cursorPos, 'end');
+        updateCode();
+        return;
+    }
+});
+
+tArea.addEventListener('keydown', function (e) {
+
+    if (e.key === "Tab" && !e.shiftKey &&
+        tArea.selectionStart == tArea.selectionEnd) {
+        e.preventDefault();
+        let cursorPosition = tArea.selectionStart;
+        let newValue = tArea.value.substring(0, cursorPosition) + "    " +
+        tArea.value.substring(cursorPosition);
+        tArea.value = newValue;
+        tArea.selectionStart = cursorPosition + 4;
+        tArea.selectionEnd = cursorPosition + 4;   
+        updateCode();
+        return;
+    }
+});
+
+
+tArea.addEventListener('keydown', function (e) {
+
+if (e.key === "Tab" && e.shiftKey &&
+
+    tArea.selectionStart == tArea.selectionEnd) {
+    e.preventDefault();
+    let cursorPosition = tArea.selectionStart;
+    let leadingSpaces = 0;
+    for (let i = 0; i < 4; i++) {
+        if (tArea.value[cursorPosition - i - 1] === " ") {
+            leadingSpaces++;
+        } else {
+            break;
+        }
+    }
+
+    if (leadingSpaces > 0) {
+        let newValue = tArea.value.substring(0, cursorPosition - leadingSpaces) +
+        tArea.value.substring(cursorPosition);
+        tArea.value = newValue;
+        tArea.selectionStart = cursorPosition - leadingSpaces;
+        tArea.selectionEnd = cursorPosition - leadingSpaces;
+    }
+
+
+    updateCode();
+    return;
+}
+
+})
+
+
 
 document.addEventListener("DOMContentLoaded", function () {
     fetch('./fetchJsonFiles').then(response => response.json()).then(files => {
