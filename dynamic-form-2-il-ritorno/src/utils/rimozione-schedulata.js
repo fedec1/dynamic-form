09ff8d66abb76113454aea07105f752db9875b35
fedec1 <colantuono.federico.w@gmail.com>
2025-03-05 09:39:55 +0100

    sort forzato su rimozione schedulata

--- dynamic-form-2-il-ritorno/src/utils/rimozione-schedulata.js
+++ dynamic-form-2-il-ritorno/src/utils/rimozione-schedulata.js
@@ -5,9 +5,16 @@ const inputJsonPath = path.join(__dirname, '../json-output')
 
 function removeMany(howMany){
     fs.readdir(inputJsonPath, (err, files) =>{
+
+      let sorted = files.sort(function(a, b){
+        if(parseInt(a.split('-')[0]) < parseInt(b.split('-')[0])) { return -1; }
+        if(parseInt(a.split('-')[0]) > parseInt(b.split('-')[0])) { return 1; }
+        return 0;
+    })
+
         for (let i = 0; i<howMany; i++) {
-            console.log(files[i])
-            fs.unlink(path.join(inputJsonPath, files[i]), (err) => {
+            console.log(sorted[i])
+            fs.unlink(path.join(inputJsonPath, sorted[i]), (err) => {
               if (err) throw err;
             })
     }
